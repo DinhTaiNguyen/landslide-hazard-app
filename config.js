950ffc500cd37fa68c526661394ee512fc51a659
DinhTaiNguyen <dinh.nguyen1@monash.edu>
2026-04-09 16:32:20 +1000

    change the api

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = window.APP_CONFIG || {
-  API_BASE_URL: 'https://landslide-hazard-app-11015728223.europe-west1.run.app'
+  API_BASE_URL: 'https://landslide-hazard-app-11015728223.australia-southeast1.run.app'
 };
